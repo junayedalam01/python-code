c69aa8c8b59f2858ef8cbcd41908b901322c7912
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-17 17:21:59 -0700

    Create fastcodes.py

---
+++ fastcodes.py
@@ -0,0 +1,104 @@
+#a = "hello world";
+#b = 4
+#c  = 3.3;
+#d = 33.33
+#e = False;
+#print (type(a));
+#print (type(b));
+#print(type(c));
+#print(type(d));
+#print(type(e));
+
+
+#print(10//3);
+#a = 10;
+#b = 20;
+#print(a==10 and a>b);
+
+#a = "bittu"
+#print('k' not in a);
+
+#b =[ 12,10,11,13,24]
+#print(107 not in b)
+
+#x =10;
+#y = 10
+#print(x is   y, x==y)
+
+#x =10;
+#y =8;
+#print(bin(x));
+#rint(bin(y));
+#print(bin(x&y),x&y);
+#print (bin(x|y),x|y)
+#print(bin(x^y),x^y)
+
+
+#tata types in python ?
+#a = 5;
+#b = 5.5;
+#c= 4+9j;
+#d = "bittu";
+#e = False
+#f='''bittu
+#ytrrtr
+#6y76uyt''';
+#x=[1,2,3,'bit']
+#x[2]=1
+#print("data types a" ,type(a));
+#print("data types b" ,type(b));
+#print("data types c" ,type(c));
+#print("data types d" ,type(d));
+#print("data types e" ,type(e));
+#print(f)
+#print(x,type(x))
+
+#b =1000
+#a = (10,20, "bittu");
+#print(a,type(a))
+#print(bin(b))
+
+
+#x ={
+ #  'your_fast_name' :'bittu01',
+  # 'your_last_name' :'alam'
+
+
+#}
+#print(x['your_fast_name'])
+#print(x,type(x));
+
+
+#y ={12,13,14,15,12}
+#print(y,type(y))
+
+# a =eval(input("Enter the value "));
+# b =eval(input("Enter the value "));
+# print("your sum" ,a+b);
+
+# a=int(input("Enter the value "))
+
+
+# if a % 2 == 0:
+#     print(a, "your number is even")
+#     print("bittu")
+# else:
+#     print(a, "your number is not even")
+
+
+# age =int(input("enter your age "))
+
+
+# if age >=60:
+#           print("you neieed silen")
+
+# elif age>=20:
+#     print("your eth is a te")
+# elif age >=18:
+#         print("you eth is aadilt" )
+        
+
+# elif age >=10:
+#            print("you nieed whater")         
+# else:
+#            print("bash milk"

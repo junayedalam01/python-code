42454b4a4802be77d51e39403532d483bc880cf0
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-17 22:21:39 -0700

    Create ro.py

---
+++ ro.py
@@ -0,0 +1,17 @@
+name= input("Enter your nmae:- ")
+if name =="bittu":
+    print("your roll","1")
+ 
+ 
+elif name=="junayed":
+     print("your roll","2")
+    
+elif name=="ov":
+    
+   print("your roll","10")
+   
+elif name =="tasif":
+    print("yuor roll","20")
+     
+else:
+    print("not fund your name ??????????")

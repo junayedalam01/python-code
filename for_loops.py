9777fffbe72deda95d4d7adecdf45e2db0d92feb
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-17 17:54:17 -0700

    Create for_loops.py

---
+++ for_loops.py
@@ -0,0 +1,18 @@
+ #for loops #
+    
+for n in range(10):
+    print("bittu")
+    print()
+    
+for n in range(1,6):
+    print(n)
+    print()
+for n in range (1,6,3):
+    print(n)
+    print()
+    for a in range(1,11):
+        print("2+" ,a, "=",2+a)
+        print()
+        
+    for b in range(10,0,-4):
+        print(b)

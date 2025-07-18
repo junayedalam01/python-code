d5395e4a756b199ec2d23666e5c6129806594c90
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-17 17:14:08 -0700

    Create Calculator_in_Python.py

---
+++ Calculator_in_Python.py
@@ -0,0 +1,23 @@
+fast1 =int(input("Enter the fast value :- "))
+fast2 =int(input("Enter the scand value :- "))
+ans=input("your /,*,+,-, %,:-?  ")
+
+if ans =="+":
+    print("your sum ",fast1+fast2)
+    
+    
+elif ans =="/":
+    print("your ans ",fast1/fast2)
+    
+    
+elif ans =="-":
+    print("your ans ",fast1-fast2)
+    
+elif ans =="*":
+    print("your ans ",fast1*fast2)
+    
+elif ans =="%":
+    print("your ans",fast1%fast2)
+    
+else:
+      print("error ?????????????")

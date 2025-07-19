6e956add4b42d83908829e3fe79352ef90e6df1f
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-18 21:26:16 -0700

    Update String_Functions.py

--- String_Functions.py
+++ String_Functions.py
@@ -13,3 +13,20 @@ print(c)
 aa="junayed alam biswas"
 u=aa.upper()
 print(u)
+
+
+#pari -2
+
+
+a ="Welcome11234567"
+b ="123456789"
+print(a.find('e',2))
+print("")
+print(a.index('e',2))
+print("")
+print(a.isalpha())
+print("")
+print(b.isdigit())
+print("")
+c="bittu@123"
+print(c.isalnum()) #spac cont and spsal cunt

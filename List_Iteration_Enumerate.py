90573c3ec78b926f7ce1bd2828618fea69de9775
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-19 00:34:48 -0700

    Create List_Iteration_Enumerate.py

---
+++ List_Iteration_Enumerate.py
@@ -0,0 +1,23 @@
+i =[12,34,56,89,90,80,34]
+t=len(i)
+print(t)
+for n in range(t):
+    print(i[n])
+    print("===")
+for a in i:
+    print(a)
+    
+    
+print("")
+
+b =[1,2,3,4,5,6,7,8,9,]
+y=len(b)
+for u in b:
+    print(u)
+    
+    
+    print("")
+
+#reverse
+for h in range(y-1,-1,-1):
+    print(b[h])

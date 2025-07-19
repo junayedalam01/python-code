3f61bc82db355d3fcf59403b3d142f3e7229f21d
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-18 22:26:39 -0700

    Create  String_Format.py

---
+++ String_Format.py
@@ -0,0 +1,14 @@
+A = "Welcome to {} home {} ".format("my", "bittu")
+print(A)
+
+a = "welcame {0} to {1} home".format("hello",20)
+print(a)
+b ="welcanme {b} to {b} bittu".format(a=30,b=40)
+print(b)
+b1 ="welcanme {a:10} to {b} bittu".format(a=30,b=40)
+print(b1)
+
+b1b="welcanme {a:^10} to {b} bittu".format(a=30,b=40)
+print(b1b)
+b1bb="welcanme {a:>10} to {b} bittu".format(a=30,b=40)
+print(b1bb)

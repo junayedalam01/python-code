82a5b3cd2ff735d8002c25a056ff16a6c3e15b2b
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-19 08:45:40 -0700

    Create Zip Function Work & How to Iterate Over 2+ Lists at the Same Time.py

---
+++ Zip Function Work & How to Iterate Over 2+ Lists at the Same Time.py
@@ -0,0 +1,15 @@
+# Zip Function Work & How to Iterate Over 2+ Lists at the Same Time
+
+
+l=[11,22,33,44,67]
+ll=[12,13,14,14,66,]
+t=len(l)
+for a,b in zip(l,ll):
+    print(a,b)
+for h in range(t):
+    print(l[h],ll[h])
+    
+
+
+
+    

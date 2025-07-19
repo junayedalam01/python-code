462d92f6d810283930b67bffcd8896553f484ad1
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-18 18:52:53 -0700

    Create  String_Functions.py

---
+++ String_Functions.py
@@ -0,0 +1,15 @@
+ # String Functions | Lower, Upper, Title & Capitalise Functions in Python?
+
+
+
+a = "JUNAYED ALAM BISWAS"
+b=a.lower() 
+print(b)
+y=a.title()
+print(y)
+c=a.capitalize()
+print(c)
+
+aa="junayed alam biswas"
+u=aa.upper()
+print(u)

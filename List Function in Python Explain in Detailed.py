97b540507e73698a95bab4f771faacf65106766f
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-19 08:36:54 -0700

    Create  List Function in Python Explain in Detailed.py

---
+++ List Function in Python Explain in Detailed.py
@@ -0,0 +1,38 @@
+"""
+There are also practical examples given in the video by our expert so that you can understand the concepts better. There are several list functions discussed in the previous part of the full series on Python.
+
+In this video, ou trainer will take you through these functions:
+
+- count() list function in Python: it will count the string.
+- max() list function in Python: it will show the maximum value.
+- min() list function in Python: it will show the minimum value.
+- sort() list function in Python: it will sort the values in ascending or descending order.
+- reverse() list function in Python: it will reverse the output.
+- index() list function in Python: it will add the index.
+
+It will solve your queries like how to reverse a list and how to perform Python list sorting."""
+
+
+#how to use in - count()
+l= [1,2,3,1,4,1,]
+print(l)
+a=l.count(1)
+print(a)
+
+#how to use in - max() max use hiest value print
+m=max(l)
+print (m)
+# how to use min() lowto  value print
+o=min(l)
+print(o)
+#how to sort() low to hiset value 
+s=[99,77,88,66,55,12,66,1,98] 
+s.sort()
+print(s)
+#how to reverse()
+s.reverse()
+print(s)
+
+#how to use index()
+v=s.index(88)
+print(v)

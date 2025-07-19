7cc8053db9987f909ca484b7cd2cb70e705f4839
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-19 00:05:12 -0700

    Create Create_List_in_Python.py

---
+++ Create_List_in_Python.py
@@ -0,0 +1,20 @@
+
+a=[1,2,3,4,5,6,7,[16,34,23,45,],8,]
+       #------------
+print(type(a))
+print(a[7][1])
+print("---")
+ #index value :- 9-,8-,-7,-6,5-,4-,-3,-2,-1
+b=[2,3,"hello",[32,43,65]]
+#index value  0 1 2 3 
+print(b[2])
+print("---")
+print(b[0:2])
+print("---")
+print(b[2:]) #2 condisun 
+print("--")
+i=[1,2,3,4,5,6,7,8,9]
+print(i[0::2])
+print("---")
+#reverse
+print(i[-1::-1])

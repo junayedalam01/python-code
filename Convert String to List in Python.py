832be70f2592ac8a75099835901852426ec81b36
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-19 17:24:53 -0700

    Create  Convert String to List in Python.py

---
+++ Convert String to List in Python.py
@@ -0,0 +1,17 @@
+
+
+#How to Convert String to List in Python Program
+n=input("Enter the value ")
+
+print(n)
+l=n.split();
+print(l)
+
+""" 
+"""
+l=[]
+for a in range(1,4):
+    n=input("enter the value" +str(a)+":-")
+    l.append(n)
+    print(l)
+    

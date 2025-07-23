2ab1f2ec8a23893147190740293b2dfb5fa29717
JUNAYED ALAM BISWAS <160825002+junayedalam01@users.noreply.github.com>
2025-07-23 01:20:18 -0700

    Create file handing codes.py

---
+++ file handing codes.py
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+
+#file delete codes
+
+
+
+"""import os 
+if os.path.exists("fast.txt"):
+      
+
+    os.remove("fast.txt")
+    print("file delete")
+else:
+    print("not file")"""
+
+
+
+
+
+
+
+
+
+#data write codes
+
+
+"""try:
+    with open("C:\\Users\\USER\\Desktop\\pj\\fast.txt", "r") as F2, \
+         open("C:\\Users\\USER\\Desktop\\pj\\fast1.txt", "w") as F3:
+        for value in F2:
+            F3.write(value)
+
+except:
+    print("not available in opath:",)
+
+else:
+    print("file closed successfully ............!")"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+#file handing codes 
+
+
+"""try:
+    a=open("C:\\Users\\USER\\Desktop\\pj\\fast10.txt",)
+
+ 
+except:
+        print("file not exgite")
+     
+else:
+     a.close()   
+     print("file exite")"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a=int(input("Enter fast value:- "))
+# b=int(input("Enter Second value:- "))
+# try:
+#     c=a/b
+# except:
+#     print("hello world")
